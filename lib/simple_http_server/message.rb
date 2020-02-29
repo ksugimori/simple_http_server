344a4de021591bd1726449a863dd085d77c8d4a6
ksugimori <ksugimori.private@gmail.com>
2020-02-29 18:36:27 +0900

    change http_method to Symbols

--- lib/simple_http_server/message.rb
+++ lib/simple_http_server/message.rb
@@ -4,22 +4,28 @@ module SimpleHttpServer
     REQUEST_LINE_PATTERN = /^(?<method>\S+) (?<target>\S+) (?<version>\S+)$/
     CRLF = "\r\n"
 
+    HTTP_METHOD = [:get, :put, :post, :patch, :delete, :head]
+
     # リクエストラインをパースして Request オブジェクトを生成する
     def self.parse_request(request_line)
       m = REQUEST_LINE_PATTERN.match(request_line)
 
       raise ParseException, "cannot parse request line: #{request_line}" if m.nil?
 
-      HttpRequest.new(m[:method], m[:target], m[:version])
+      method = m[:method].downcase.to_sym
+      target = m[:target]
+      version = m[:version]
+
+      HttpRequest.new(method, target, version)
     end
 
     # HTTP リクエスト
     class HttpRequest
-      attr_reader :http_method, :request_uri, :version
+      attr_reader :http_method, :target, :version
 
-      def initialize(http_method, request_uri, version)
+      def initialize(http_method, target, version)
         @http_method = http_method
-        @request_uri = request_uri
+        @target = target
         @version = version
       end
     end
--- spec/simple_http_server/message_spec.rb
+++ spec/simple_http_server/message_spec.rb
@@ -4,16 +4,30 @@ describe SimpleHttpServer::Message::HttpRequest do
   it "can parse GET request" do
     request_line = "GET /path/to/resource HTTP/1.1"
     request = SimpleHttpServer::Message.parse_request(request_line)
-    expect(request.http_method).to eql("GET")
-    expect(request.request_uri).to eql("/path/to/resource")
+    expect(request.http_method).to eql(:get)
+    expect(request.target).to eql("/path/to/resource")
     expect(request.version).to eql("HTTP/1.1")
   end
 
   it "can also parse HTTP/1.0 GET request" do
     request_line = "GET /path/to/another HTTP/1.0"
     request = SimpleHttpServer::Message.parse_request(request_line)
-    expect(request.http_method).to eql("GET")
-    expect(request.request_uri).to eql("/path/to/another")
+    expect(request.http_method).to eql(:get)
+    expect(request.target).to eql("/path/to/another")
     expect(request.version).to eql("HTTP/1.0")
+  end
+
+  it "can parse methods" do
+    put_request = SimpleHttpServer::Message.parse_request("PUT /hoge HTTP/1.1")
+    post_request = SimpleHttpServer::Message.parse_request("POST /hoge HTTP/1.1")
+    patch_request = SimpleHttpServer::Message.parse_request("PATCH /hoge HTTP/1.1")
+    delete_request = SimpleHttpServer::Message.parse_request("DELETE /hoge HTTP/1.1")
+    head_request = SimpleHttpServer::Message.parse_request("HEAD /hoge HTTP/1.1")
+
+    expect(put_request.http_method).to eql(:put)
+    expect(post_request.http_method).to eql(:post)
+    expect(patch_request.http_method).to eql(:patch)
+    expect(delete_request.http_method).to eql(:delete)
+    expect(head_request.http_method).to eql(:head)
   end
 end
